d39c941c20c45bbf48676f8cbbf0132df8735d9c
fugibeast <rarroyo8@calstatela.edu>
2016-11-25 08:27:20 -0800

    Update Transactions.java

--- GroupProj/Transactions.java
+++ GroupProj/Transactions.java
@@ -27,8 +27,8 @@ public class Transactions extends HttpServlet {
         try
         {
             String url = "jdbc:mysql://cs3.calstatela.edu/cs3220stu27";
-            String username = "cs3220stu27";
-            String password = "t1##a*!#";
+            String username = "";
+            String password = "";
 
             c = DriverManager.getConnection( url, username, password );
             Statement stmt = c.createStatement();
