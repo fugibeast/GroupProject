c21256d907a6d6f3fd11d598cdc28594c66ab2a8
fugibeast <rarroyo8@calstatela.edu>
2016-11-26 18:20:35 -0800

    Update Edit.java

--- Controllers/Edit.java
+++ Controllers/Edit.java
@@ -25,9 +25,9 @@ public class Edit extends HttpServlet {
 		Connection c = null;
         try
         {
-            String url = "jdbc:mysql://cs3.calstatela.edu/cs3220stu27";
-            String username = "cs3220stu27";
-            String password = "t1##a*!#";
+            String url = "jdbc:mysql://cs3.calstatela.edu/cs3220stu";
+            String username = "";
+            String password = "";
 
             c = DriverManager.getConnection( url, username, password );
             Statement stmt = c.createStatement();
@@ -84,9 +84,9 @@ public class Edit extends HttpServlet {
 			Connection c = null;
 	        try
 	        {
-	            String url = "jdbc:mysql://cs3.calstatela.edu/cs3220stu27";
-	            String username = "cs3220stu27";
-	            String password = "t1##a*!#";
+	            String url = "jdbc:mysql://cs3.calstatela.edu/cs3220stu";
+	            String username = "";
+	            String password = "";
 	            
 	            String sql = "update inventory set name=?, price=?, quantity=?, description=? where id=?";;
 	            c = DriverManager.getConnection( url, username, password );
